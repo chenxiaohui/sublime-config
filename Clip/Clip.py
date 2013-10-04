1ff13736e9d2d89733e79657e0238ebcc6f1dc2c
HarryChen <sdqxcxh@gmail.com>
2013-10-04 13:41:05 +0800

    modified clip

--- Clip/Clip.py
+++ Clip/Clip.py
@@ -3,5 +3,5 @@ import sublime, sublime_plugin,os
 class ClipsshCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		os.system("~/repo/scripts/clip.sh")
-		self.view.insert(edit, 0, sublime.get_clipboard())
+		self.view.insert(edit, self.view.sel()[0].b, sublime.get_clipboard())
 
