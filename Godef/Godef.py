76a047f3f4913532d655d4371ecdf81bc742c850
chenxiaohui <sdqxcxh@gmail.com>
2015-01-05 09:59:22 +0800

    modify snippet

--- Godef/Godef.py
+++ Godef/Godef.py
@@ -1,90 +1,86 @@
-# This plugin is based on [GoTools](https://github.com/ironcladlou/GoTools) which is created by [Dan Mace](https://github.com/ironcladlou) and it's under MIT license:
-
-# The MIT License (MIT)
-
-# Copyright (c) 2014 Dan Mace
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
 import sublime, sublime_plugin, subprocess, os, time
 
 class GodefCommand(sublime_plugin.WindowCommand):
   def run(self):
+    print("=================[Godef]Begin=================")
     settings = sublime.load_settings("Godef.sublime-settings")
-    self.gopath = settings.get("gopath", os.getenv('GOPATH'))
-
-    if self.gopath is None:
-      self.log("ERROR: no GOPATH defined")
+    gopath = settings.get("gopath", os.getenv('GOPATH'))
+    if gopath is None:
+      print("[Godef]ERROR: no GOPATH defined")
+      print("=================[Godef] End =================")
       return
 
-    self.log("using gopath: ", self.gopath)
+    gopaths = gopath.split(":")
+    found = False
+    godefpath = ""
+    for path in gopaths:
+      godefpath = os.path.join(path, "bin", "godef")
+
+      if not os.path.isfile(godefpath):
+        print("[Godef]WARN: godef not found at" + godefpath)
+        continue
+      else:
+        print("[Godef]INFO: godef found at" + godefpath)
+        found = True
+        break
+
+    if found == False:
+      print("[Godef]ERROR: godef not found!")
+      print("=================[Godef] End =================")
+      return
+    else:
+      print("[Godef]INFO: using godef:" + godefpath)
 
     view = self.window.active_view()
-    row, col = view.rowcol(view.sel()[0].begin())
 
-    self.offset = view.text_point(row, col)
-    self.filename = view.file_name()
+    # row, col = view.rowcol(view.sel()[0].begin())
 
-    sublime.set_timeout_async(self.godef, 0)
+    # offset = view.text_point(row, col)
 
-  def godef(self):
-    godef_bin = os.path.join(self.gopath, "bin", "godef")
+    view = self.window.active_view()
+    select = view.sel()[0]
+    select_begin = select.begin()
+    select_before = sublime.Region(0, select_begin)
+    string_before = view.substr(select_before)
+    string_before.encode("utf-8")
+    buffer_before = bytearray(string_before, encoding = "utf8")
+    offset = len(buffer_before)
+    print("[Godef]INFO: selcet_begin: " + str(select_begin) + " offset: " + str(offset))
 
-    if not os.path.isfile(godef_bin):
-      self.log("ERROR: godef not found at", godef_bin)
-      return
+    filename = view.file_name()
 
-    try:
-      args = [
-        os.path.join(self.gopath, "bin", "godef"),
-        "-f",
-        self.filename,
-        "-o",
-        str(self.offset)
-      ]
+    args = [
+      godefpath,
+      "-f",
+      filename,
+      "-o",
+      str(offset)
+    ]
 
-      self.log("spawning", " ".join(args))
+    print("[Godef]INFO: spawning: " + " ".join(args))
 
-      env = os.environ.copy()
-      env["GOPATH"] = self.gopath
-      output = subprocess.check_output(args, stderr=subprocess.STDOUT, env=env)
-    except subprocess.CalledProcessError as e:
-      self.log("no definition found: ", e)
+    env = os.environ.copy()
+    env["GOPATH"] = gopath
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+    output, stderr = p.communicate()
+    if stderr:
+      print("[Godef]ERROR: no definition found: " + str(stderr))
+      print("=================[Godef] End =================")
       return
 
     location = output.decode("utf-8").rstrip().split(":")
 
-    file = location[0]
-    row = int(location[1]) - 1
-    col = int(location[2])
+    if len(location) == 3:
+      print("[Godef]INFO: godef output: " + str(output))
+      file = location[0]
+      row = int(location[1])
+      col = int(location[2])
 
-    self.log("opening definition at " + file + ":" + str(row) + ":" + str(col))
-    view = self.window.open_file(file)
+      postion = (file + ":" + str(row) + ":" + str(col))
+      print("[Godef]INFO: opening definition at " + postion)
+      view = self.window.open_file(postion, sublime.ENCODED_POSITION)
+      # view.show_at_center(region)
+    else:
+      print("[Godef]ERROR: godef output bad: " + str(output))
+    print("=================[Godef] End =================")
 
-    while view.is_loading():
-      time.sleep(0.01)
-
-    region = sublime.Region(view.text_point(row, 0))
-    view.sel().clear()
-    view.sel().add(region)
-    view.show_at_center(region)
-
-  def log(self, *messages):
-    print("[Godef]", *messages)
